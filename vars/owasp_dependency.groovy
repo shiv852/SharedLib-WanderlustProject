b18a0de0bd54a94886477fa82122489169bc21b5
shiv kumar saini <141953893+shiv852@users.noreply.github.com>
2025-09-08 17:21:58 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,20 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
-  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+def call() {
+    // Install dependencies before scanning
+    dir('backend') {
+        sh 'npm install'
+    }
+    dir('frontend') {
+        sh 'npm install'
+    }
+
+    // Run Dependency-Check
+    dependencyCheck additionalArguments: '''
+        --disableNodeAudit 
+        --scan ./backend 
+        --scan ./frontend 
+        --format XML
+    ''', odcInstallation: 'OWASP'
+
+    // Publish the results
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
