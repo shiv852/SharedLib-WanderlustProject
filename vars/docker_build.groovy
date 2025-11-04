d7ce1d2706fd1a1e4838dd9ad5bddd99d1ce3722
shiv kumar saini <141953893+shiv852@users.noreply.github.com>
2025-11-04 17:03:18 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,31 +1,23 @@
 // ye wanderlust project ki image build krne ki hai
 
-// def call(String ProjectName, String ImageTag, String DockerHubUser){
-//   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
-// }
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+}
 
 
-// ye own-furniture-web ki image build krne ki hai
+
+
+
+
 // def call(String ProjectName, String ImageTag, String DockerHubUser){
+//     ImageTag = ImageTag.trim()
+
 //     if (ProjectName == "furniture-backend") {
-//         sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} -f backend/Dockerfile backend"
+//         sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} -f Dockerfile ."
 //     } else if (ProjectName == "furniture-frontend") {
-//         sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} -f finalProject/Dockerfile finalProject"
+//         sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} -f Dockerfile ."
 //     }
 // }
-
-
-
-
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-    ImageTag = ImageTag.trim()
-
-    if (ProjectName == "furniture-backend") {
-        sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} -f Dockerfile ."
-    } else if (ProjectName == "furniture-frontend") {
-        sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} -f Dockerfile ."
-    }
-}
 
 
 
