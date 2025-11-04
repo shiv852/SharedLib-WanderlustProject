26ff5a6695a6adee0ae698e438b1c20b382bbd36
shiv kumar saini <141953893+shiv852@users.noreply.github.com>
2025-11-04 15:59:22 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,14 +1,17 @@
 // ye wanderlust project ki image build krne ki hai
 
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
-}
+// def call(String ProjectName, String ImageTag, String DockerHubUser){
+//   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+// }
 
 
 // ye own-furniture-web ki image build krne ki hai
-// def call(String ProjectName, String ImageTag, String DockerHubUser) {
-//     sh """
-//         echo "🛠 Building Docker image for ${ProjectName}:${ImageTag}"
-//         DOCKER_BUILDKIT=0 docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} .
-//     """
-// }
+def call(String ProjectName, String ImageTag, String DockerHubUser){
+    if (ProjectName == "furniture-backend") {
+        sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} -f backend/Dockerfile backend"
+    } else if (ProjectName == "furniture-frontend") {
+        sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} -f finalProject/Dockerfile finalProject"
+    }
+}
+
+
